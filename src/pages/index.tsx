0854917903ae3731eb86fc5ada0195d87b5b6e2e
yoav <yoavdan.m@gmail.com>
2023-01-09 13:22:21 +0200

    generative art noobie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Outlet, Link } from 'react-router-dom'
 
 const links = [
-  { url: '/01', title: 'Infinite tags', number: '01' },
-  { url: '/02', title: "Antfu's plum", number: '02' },
-  { url: '/03', title: 'DVD', number: '02' },
+  { url: '/tilling', title: 'tiles', number: '01' },
+  { url: '/dvd', title: 'DVD', number: '02' },
+  { url: '/plum', title: "Antfu's plum", number: '03' },
 ]
 
 export default function () {
@@ -14,7 +14,10 @@ export default function () {
           <h1 className="font-mono font-bold mb-4 text-lg ">100 Challenge</h1>
           <pre className="grid gap-y-1 gap-x-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {links.map(link => (
-              <li className="text-gray-400  block mr-4 list-none hover:text-gray-700 duration-150 ease-in-out group/link">
+              <li
+                key={link.number}
+                className="text-gray-400  block mr-4 list-none hover:text-gray-700 duration-150 ease-in-out group/link"
+              >
                 <Link to={link.url}>
                   <span className="text-gray-300 group-hover/link:text-gray-400 duration-150 ease-in-out mr-2">
                     {link.number}
