92d6b378360ac940ebc9bbf4637470905f52ab3b
yoav <yoavdan.m@gmail.com>
2023-01-10 00:23:31 +0200

    favicon

--- src/pages/cubic-disarray.tsx
+++ src/pages/cubic-disarray.tsx
@@ -1,4 +1,4 @@
-import { initCanvas, r90, plusOrMinus } from '../utils'
+import { initCanvas, r90, plusOrMinus, randomColor } from '../utils'
 import { Link } from 'react-router-dom'
 import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { useState, useRef, useEffect } from 'react'
@@ -12,14 +12,15 @@ function draw(
   for (let i = size; i <= w - size; i += size) {
     for (let j = size; j <= w - size; j += size) {
       let rotateAmt =
-        (((j / w) * Math.PI) / 90) * plusOrMinus() * Math.random() * (size / 5)
-      let translateAmt = ((j / w) * plusOrMinus() * Math.random() * size) / 5
+        (((j / w) * Math.PI) / 90) * plusOrMinus() * Math.random() * (size / 2)
+      let translateAmt = ((j / w) * plusOrMinus() * Math.random() * size) / 2
       ctx.save()
       ctx.translate(i + translateAmt + size / 3, j + size / 3)
       ctx.rotate(rotateAmt)
       //   ctx.rotate(rotateAmt)
       //   ctx.rotate(rotateAmt)
       ctx.beginPath()
+
       ctx.rect(-size / 2, -size / 2, size, size)
       ctx.stroke()
       ctx.restore()
@@ -29,7 +30,7 @@ function draw(
 
 export default function () {
   const el = useRef<HTMLCanvasElement | null>(null)
-  const [nCubes, setNCubes] = useState(9)
+  const [nCubes, setNCubes] = useState(19)
 
   useEffect(() => {
     const canvas = el.current!
@@ -37,7 +38,7 @@ export default function () {
     const { width, height } = canvas
 
     draw(ctx, width / nCubes, width, height)
-  }, [])
+  }, [nCubes])
   return (
     <>
       <div className="page">
@@ -45,7 +46,11 @@ export default function () {
           <ChevronLeftIcon className="w-6 h-6" />
         </Link>
         <main className="centered">
-          <canvas className="canvas" ref={el}></canvas>
+          <canvas
+            onClick={() => setNCubes((nCubes % 20) + 1)}
+            className="canvas"
+            ref={el}
+          ></canvas>
         </main>
       </div>
     </>
